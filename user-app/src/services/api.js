95698102377965449c57bceb9c53f1de34ca4bc3
shettyprathamp <shettyprathamp147@gmail.com>
2026-08-28 22:18:47 +0530

    Connect frontend to deployed backend

--- user-app/src/services/api.js
+++ user-app/src/services/api.js
@@ -1,7 +1,8 @@
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://project-atlas-backend-qxpb.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
