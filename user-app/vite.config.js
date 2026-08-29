308c0f9e6df7574bbaf91c511301b8dce2cc32b0
shettyprathamp <shettyprathamp147@gmail.com>
2026-08-29 14:55:19 +0530

    Allow Render frontend host

--- user-app/vite.config.js
+++ user-app/vite.config.js
@@ -3,5 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   base: "./",
+
+  preview: {
+    allowedHosts: [
+      "project-atlas-frontend-r3ce.onrender.com",
+    ],
+  },
 });
